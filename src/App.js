787050e6a37b24af9ea6edffb0054bd95cbd4908
monukr2310 <monukr377@gmail.com>
2021-03-11 21:51:20 +0530

    new changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Welcome from "./Welcome";
+
+export default function App() {
+  return (
+         <React.Fragment>
+             <Welcome   name="Prince Kumar"/>
+
+         </React.Fragment>
+  );
+}
+
+//export default App;
