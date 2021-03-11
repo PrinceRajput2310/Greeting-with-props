225377af1c869188bf46c9bb536e2dfef9180e08
monukr2310 <monukr377@gmail.com>
2021-03-11 22:06:20 +0530

    change3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Welcome from "./Welcome";
 export default function App() {
   return (
          <React.Fragment>
-             <Welcome   name="Prince Kumar"/>
+             <Welcome   name="Monu Kumar"/>
 
          </React.Fragment>
   );
